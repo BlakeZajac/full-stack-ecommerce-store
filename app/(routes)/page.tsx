3378343beb663cb4bdc69088cca8b28ba950f6c8
Blake Zajac <contact@blakezajac.com>
2023-07-29 00:26:50 +1000

    Set up products API from admin

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,23 @@
-const HomePage = () => {
+import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("f5842ac5-402a-4bd9-8ed0-cd2eb92f0ef2");
+
   return (
-    <div>
-      <div>
-        <div>Home page - Ecommerce store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
       </div>
-    </div>
+
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured products" items={products} />
+      </div>
+    </Container>
   );
 };
 
---
+++ components/ui/product-card.tsx
@@ -0,0 +1,15 @@
+interface ProductCardProps {}
+
+const ProductCard: React.FC<ProductCardProps> = ({}) => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>Product Card</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
