bda128d06686fe5cfde78754b0c81c0ba4d1b74e
Blake Zajac <contact@blakezajac.com>
2023-07-29 13:06:43 +1000

    Set up cart cards

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -31,6 +31,21 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
         <div className="absolute z-10 right-0 top-0">
           <IconButton onClick={() => {}} icon={<X size={15} />} />
         </div>
+
+        <div className="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
+          <div className="flex justify-between">
+            <p className="text-lg font-semibold text-black">{data.name}</p>
+          </div>
+
+          <div className="md:mt-1 mb-2 flex text-sm">
+            <p className="text-gray-500">{data.color.name}</p>
+            <p className="text-gray-500 ml-2 border-l border-gray-200 pl-2">
+              {data.size.name}
+            </p>
+          </div>
+
+          <Currency value={data.price} />
+        </div>
       </div>
     </li>
   );
